9900d5e1a34886d72d8c9efb4d9740ccb3c96e85
Navaru <eugen.tudorancea@gmail.com>
2012-07-01 03:00:03 +0300

    Added grunt and package.json

---
+++ grunt.js
@@ -0,0 +1,66 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+// ----------------------------------------------------------------------------
+//  Project configuration
+// ----------------------------------------------------------------------------
+  grunt.initConfig({
+    pkg: '<json:package.json>'
+  , meta: {
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    }
+  , lint: {
+      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+    }
+  , concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:lib/<%= pkg.name %>.js>']
+      , dest: 'dist/<%= pkg.name %>.js'
+      }
+    }
+  , min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>']
+      , dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    }
+  , watch: {
+      files: '<config:lint.files>'
+    , tasks: 'lint'
+    }
+  , jshint: {
+      options: {
+        curly: true
+      , eqeqeq: true
+      , immed: true
+      , latedef: true
+      , newcap: true
+      , noarg: true
+      , sub: true
+      , undef: true
+      , boss: true
+      , eqnull: true
+      , browser: true
+      , laxcomma: true
+      }
+    , globals: {}
+    }
+  , uglify: {}
+  });
+
+
+// ----------------------------------------------------------------------------
+//  Loading grunt plugins or tasks folders
+// ----------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------
+//  Tasks and Helpers
+// ----------------------------------------------------------------------------
+  grunt.registerTask('default', 'lint concat min');
+
+};

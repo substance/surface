29a16e656ad867b864b7dcfb227dfff8f89b71d0
michael <michael@substance.io>
2013-07-07 01:14:10 +0200

    Handle some actions.

--- surface.js
+++ surface.js
@@ -16,17 +16,57 @@
     // Action handlers
     // --------
 
-    // this.handle('delete', this.delete);
-    // this.handle('undo', this.undo);
-    // this.handle('redo', this.redo);
-    // this.handle('copy', this.copy);
-    // this.handle('paste', this.paste);
+    this.handle('delete', this.delete);
+    this.handle('copy', this.copy);
+    this.handle('paste', this.paste);
+    this.handle('undo', this.undo);
+    this.handle('redo', this.redo);
+    
+    
   };
 
   // Does the same as
   // new View.prototype;
   Surface.prototype = Object.create(Substance.View.prototype);
   Surface.prototype.constructor = Surface;
+
+
+  // Handle Events
+  // ==========================================================================
+  // 
+
+  Surface.prototype.delete = function(e) {
+    console.log('deleting selection...', e);
+    // this.model.delete();
+    e.preventDefault();
+  };
+
+  Surface.prototype.copy = function(e) {
+    console.log('copying selection...');
+    // this.model.copy();
+    e.preventDefault();
+  };
+
+  Surface.prototype.paste = function(e) {
+    console.log('pasting at current selection...');
+    // TODO: checkout clipboard
+    var content = this.session.clipboard;
+    // this.model.paste();
+    e.preventDefault();
+  };
+
+  Surface.prototype.undo = function(e) {
+    console.log('undoing...');
+    // this.model.undo();
+    e.preventDefault();
+  };
+
+  Surface.prototype.redo = function(e) {
+    console.log('redoing...');
+    // this.model.redo();
+    e.preventDefault();
+  };
+
 
   // Rendering
   // ==========================================================================
